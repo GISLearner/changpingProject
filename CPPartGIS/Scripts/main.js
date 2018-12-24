ff36579efb1a8877b281cd8244d3ed552049d627
Jade <fanlu18@gmail.com>
2018-12-24 16:49:05 +0800

    update html

---
+++ CPPartGIS/Scripts/main.js
@@ -0,0 +1,21 @@
+﻿
+var app = {
+    init() {
+        //模糊搜索
+        $("#btnSearchNormal").click(function () { 
+            var txt = $(".entSearch_text").val(); 
+            $("#panelResult").show();
+            $("#panelCondition").hide();
+            //搜索
+        })
+        //高级搜索
+        $("#btnSearchAdvanced").click(function () { 
+            $("#panelResult").hide();
+            $("#panelCondition").show();
+        })
+    }
+}
+
+
+
+app.init();
